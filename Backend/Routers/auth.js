41069615cffd9dd32a833bd3ceff9f48f574a939
Spark <85449025+Spark-Things@users.noreply.github.com>
2022-12-18 00:36:57 +0530

    router update

--- Backend/Routers/auth.js
+++ Backend/Routers/auth.js
@@ -12,67 +12,77 @@ router.get("/checkout", requireLogin, (req, res) => {
 });
 
 router.post("/signup", (req, res) => {
-  console.log(req.body);
   const { name, email, password } = req.body;
+
+  if (
+    !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    )
+  ) {
+    return console.log("Invalid Email");
+  }
   if (!name || !email || !password) {
     return res.json({ error: "pehle dang se form bhar le jyada hero mat ban" });
-  } else {
-    bcrypt
-      .hash(password, 12)
-      .then((hashedpassword) => {
-        User.findOne({ email: email }).then((savedUser) => {
-          if (savedUser) {
-           return res.json({ Error: "Tu fir aa gaya tu jaa ree signin kar" });
-          }
-
-          const user = new User({
-            name,
-            email,
-            password: hashedpassword,
-          });
-
-          user
-            .save()
-            .then(
-              res.json({ msg: "saved user successfully" }))
-            .catch((err) => {
-              res.json({ error: err });
-            });
-        });
-      })
-
-      .catch((err) => {
-        res.json({ error: err });
-      });
   }
+  bcrypt
+    .hash(password, 12)
+    .then((hashedpassword) => {
+      User.findOne({ email: email }).then((savedUser) => {
+        if (savedUser) {
+          return res.json({ Error: "Tu fir aa gaya tu jaa ree signin kar" });
+        }
+
+        const user = new User({
+          name,
+          email,
+          password: hashedpassword,
+        });
+
+        user
+          .save()
+          .then(res.json({ msg: "saved user successfully" }))
+          .catch((err) => {
+            res.json({ error: err });
+          });
+      });
+    })
+
+    .catch((err) => {
+      res.json({ error: err });
+    });
 });
 
 router.post("/signin", (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body;
-  if (email == undefined || password == undefined) {
-    res.status(402).send("Enter valid email and password");
-  } else {
-    User.findOne({ email: email })
-      .then((savedUser) => {
-        if (!savedUser) {
-          return res.send({ msg: "Please signup first" });
-        } else {
-          bcrypt.compare(password, savedUser.password).then((doMatch) => {
-            if (doMatch) {
-              // res.json({msg : "Successfully Logged In"})
-              const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
-              res.json({ token: token });
-            } else {
-              res.json({ msg: "Enter valid email and password" });
-            }
-          });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  if (
+    !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    )
+  ) {
+    return res.json("Invalid Email");
   }
+  if (email == undefined || password == undefined) {
+    return res.status(402).send("Enter valid email and password");
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.send({ msg: "Please signup first" });
+      } else {
+        bcrypt.compare(password, savedUser.password).then((doMatch) => {
+          if (doMatch) {
+            // res.json({msg : "Successfully Logged In"})
+            const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+            res.json({ token: token });
+          } else {
+            res.json({ msg: "Enter valid email and password" });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
