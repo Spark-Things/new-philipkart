b60c9d9397b9781a2c3805c9d7a386ceb621657b
Spark <85449025+Spark-Things@users.noreply.github.com>
2022-12-18 00:37:58 +0530

    checkoutpage update

--- Frontend/src/component/Signuppage.jsx
+++ Frontend/src/component/Signuppage.jsx
@@ -17,7 +17,8 @@ function Signuppage() {
           password,
           name
       })
-    }).then(res => console.log(res.json()))
+    }).then(res => res.json())
+    .then(result => console.log(result))
     .catch(err => console.log(err))
   }
   return (
