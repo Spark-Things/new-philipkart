bc38fbe4abce0e7108431dbf84d5609acc762603
Spark <85449025+Spark-Things@users.noreply.github.com>
2022-12-23 19:06:08 +0530

    adContainer update

--- Frontend/src/component/Home.jsx
+++ Frontend/src/component/Home.jsx
@@ -1,19 +1,26 @@
 import React,{useState} from "react";
 import Panelcontainer from "../widget/panelcontainer";
+import one from "../Images/1.jpg";
+import two from "../Images/2.jpg";
+import three from "../Images/3.jpg";
 
 function Home() {
-  
-   const Adds =[
-    {link: ""},
-    {link: ""},
-    {link: ""},
-    {link: ""},
-   ] 
 
+   const [Disp, setDisp] = useState(1);
+
+   console.log(Disp);
   return (
     <div className="maincontainer">
       <div className="Adcontainer">
-        content
+        {Disp == 1 ?   <img className="ads" src={one}></img> : null}
+        {Disp == 2 ?   <img className="ads" src={two}></img> : null}
+        {Disp == 3 ?    <img className="ads" src={three}></img> : null}
+         <button style={{"position":"absolute","left":"0px"}} 
+         onClick={() => setDisp(Disp < 3 ? Disp+1 : 1)}
+         > " + "</button>
+         <button style={{"position":"absolute","left":"50px"}} 
+         onClick={() => setDisp(Disp <= 3 && Disp > 1 ? Disp-1 : 3)}
+         > " - "</button>
         </div>
         <Panelcontainer category="Shoes" />
         <Panelcontainer category="Mobile"/>
